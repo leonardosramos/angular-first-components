8891996f832c2cb5afbe738a5962be585e136ed5
Leonardo Ramos <leonardosr793@gmail.com>
2026-06-18 20:04:46 -0300

    Primeiros Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloworldComponent } from "./helloworld/helloworld.component";
+import { MinhapaginaComponent } from "./minhapagina/minhapagina.component";
+import { CalculadoraComponent } from "./calculadora/calculadora.component";
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HelloworldComponent, MinhapaginaComponent, CalculadoraComponent, ListaComprasComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'conceitos-basicos';
---
+++ src/app/lista-compras/lista-compras.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lista-compras',
+  standalone: true,
+  imports: [ FormsModule, CommonModule ],
+  templateUrl: './lista-compras.component.html',
+  styleUrls: ['./lista-compras.component.scss']
+})
+export class ListaComprasComponent {
+
+  item: string = '';
+  lista: ItemLista[] = [];
+  
+  adicionarItem(){
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
+    this.item = '';
+  }
+
+  riscarItem(itemLista: ItemLista) {
+    itemLista.comprado = !itemLista.comprado;
+  }
+
+   limparLista(){
+    this.lista = [];
+  }
+}
